074acb85ffe331ffc42247bb100766ec653af373
William Jones <williamjones120862@gmail.com>
2018-08-13 17:02:42 -0400

    Optimize views for tablet screen size

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,11 @@
 module ApplicationHelper
   def display_date(date)
-    formatted = date.strftime("%e %b %Y")
+    if date
+      formatted = date.strftime("%e %b %Y")
+    else
+      formatted = ' '
+    end
+    formatted
   end
   def human_boolean(boolean)
       boolean ? 'Yes' : 'No'
--- app/helpers/product_claims_images_helper.rb
+++
@@ -1,2 +0,0 @@
-module ProductClaimsImagesHelper
-end
